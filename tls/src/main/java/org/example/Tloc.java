0f6f7976f7d8b5138b044533afd091723bfa48b9
IbraOS <ibra12330@gmail.com>
2023-09-29 17:43:37 -0400

    tloc tassert
    update tloc tassert in tls

--- tls/src/main/java/org/example/Tloc.java
+++ tls/src/main/java/org/example/Tloc.java
@@ -1,29 +1,41 @@
 package org.example;
 
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Tloc {
     public static void main(String[] args) throws IOException {
+
+        int count = numTloc(args);
+        System.out.println(count);
+
+    }
+
+    public static int numTloc(String[] args) {
         if (args.length == 0) {
             System.out.println("Run the program again and provide the file path as an argument.");
-            return;
+            return -1;
         }
 
-        BufferedReader reader = new BufferedReader(new FileReader(args[0]));
-        int count = 0;
-        String line;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+            int count = 0;
+            String line;
 
-        while ((line = reader.readLine()) != null) {
-            line = line.trim();
-            if (!line.isEmpty() && !line.startsWith("//") && !line.startsWith("/*") && !line.startsWith("*")) {
-                count++;
+            while ((line = reader.readLine()) != null) {
+                line = line.trim();
+                if (!line.isEmpty() && !line.startsWith("//") && !line.startsWith("/*") && !line.startsWith("*")) {
+                    count++;
+                }
             }
+
+            reader.close();
+            return count;
+        } catch (Exception e) {
+            return -1;
         }
 
-        reader.close();
-        System.out.println(count);
     }
 }
+
