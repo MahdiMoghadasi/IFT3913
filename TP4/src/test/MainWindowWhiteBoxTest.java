e432c2050c6627e95e93b546be3111730a13e61f
mahdi moghadasi <mahdi.moghadasi@umontreal.ca>
2023-12-08 14:13:39 -0500

    added indepandant path analysis

--- TP4/src/test/MainWindowWhiteBoxTest.java
+++ TP4/src/test/MainWindowWhiteBoxTest.java
@@ -23,6 +23,7 @@ public class MainWindowWhiteBoxTest {
     }
 
     @Test
+    // le chemin couvert par ce test est 1 2 3 4 5 6 7 8 9 10 11 12
     public void testCurrency2Found() {
         double amount = 100.0;
         Double convertedAmount = MainWindow.convert("US Dollar", "Euro", currencies, amount);
