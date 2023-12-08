e396d25b77e1aef81d0ca4ca12bf16b6ae448f64
mahdi moghadasi <mahdi.moghadasi@umontreal.ca>
2023-12-08 18:20:59 -0500

    updating test cases currency blackbox
    Co-Authored-By: IbraOS <113637672+IbraOS@users.noreply.github.com>

--- TP4/src/test/CurrencyBlackBoxTest.java
+++ TP4/src/test/CurrencyBlackBoxTest.java
@@ -10,38 +10,52 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CurrencyBlackBoxTest {
 
-// Test valid amounts with conversion rate less than 1
+// Test valid amounts with valid conversion rate less than 1
     @ParameterizedTest
-    @ValueSource(doubles ={0.0, 1.0, 10.0, 25.0, 1000000.0})
+    @ValueSource(doubles ={0.0, 1.0, 100.0, 1000000.0})
     public void testValidAmountLessThan1Ex(double amount) {
         double result = Currency.convert(amount, 0.75);
         assertEquals(amount * 0.75, result , "Conversion valid");
     }
 
-    // Test valid amounts with conversion rate more than 1
+    // Test valid amounts with valid conversion rate more than 1
     @ParameterizedTest
-    @ValueSource(doubles ={0.0, 1.0, 10.0, 25.0, 1000000.0})
+    @ValueSource(doubles ={0.0, 1.0, 100.0, 1000000.0})
     public void testValidAmountMoreThan1Ex(double amount) {
         double result = Currency.convert(amount, 1.25);
         assertEquals(amount * 1.25, result , "Conversion valid");
     }
 
 
-    // Test an amount with negative or zero conversion rate(invalid)
-    @ParameterizedTest
-    @ValueSource(doubles ={0.0, -1.0, -1.25, -0.75})
-    public void testNegativeConversionRate(double exRate) {
-       assertThrows(IllegalArgumentException.class, () ->
-               Currency.convert(50.0, exRate));
-    }
-
-
-    // Test invalid amounts with conversion rate more than 1
+    // Test invalid amounts with valid conversion rate more than 1
     @ParameterizedTest
     @ValueSource(doubles ={-10.0, -1.0, 1000001.0, 1000010.0})
     public void testInValidAmountMoreThan1Ex(double amount) {
         assertThrows(IllegalArgumentException.class, () ->
                 Currency.convert(amount, 1.25));
     }
+
+
+    // Test valid conversion rates  with valid amount
+    @ParameterizedTest
+    @ValueSource(doubles ={ 0.1, 1, 1000000.0})
+    public void testVaidConversionRate(double exRate) {
+        double result = Currency.convert(50.0, exRate);
+        assertEquals(50 * exRate, result , "Conversion valid");
+    }
+
+
+    // Test invalid conversion rates with valid amount
+    @ParameterizedTest
+    @ValueSource(doubles ={0.0, -1.0, -1.25, -0.75})
+    public void testNegativeConversionRate(double exRate) {
+        assertThrows(IllegalArgumentException.class, () ->
+                Currency.convert(50.0, exRate));
+    }
+
+
+
+
+
 }
 
